89043102c4ce9cfcc318641058bfc419c7197c7b
alvinstefanus <astefanus17@gmail.com>
2022-10-11 15:24:27 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'OTP Example',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
